3a341853633bf32c90c875baac9668a0bdc406a7
Sean Lorman <sean.lorman@gmail.com>
2020-03-04 12:24:39 -0700

    Listen on port 3000 and filter the bugs by threatLevel

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,31 +1,39 @@
-import React from 'react';
-import Nav from './Nav.jsx';
-import BugTile from './BugTile.jsx';
-import exampleData from '../example-data/exampleData';
+import React from "react";
+import Nav from "./Nav.jsx";
+import BugTile from "./BugTile.jsx";
+import exampleData from "../example-data/exampleData";
 
-import '../styles/App.scss';
+import "../styles/App.scss";
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      filter: 'None',
-      bugs: exampleData,
+      filter: "None",
+      bugs: exampleData
     };
     this.filterHandler = this.filterHandler.bind(this);
   }
 
   filterHandler(filter) {
-    this.setState({ filter });
+    this.setState({ filter }, () =>
+      this.setState({
+        bugs: exampleData.filter(bug => bug.threatLevel === this.state.filter)
+      })
+    );
   }
+
+  // componentDidMount() {
+  //   this.setState({
+  //     bugs: exampleData.filter(bug => bug.threatLevel === this.state.filter)
+  //   });
+  // }
 
   render() {
     return (
       <table>
-        <Nav
-          filterHandler={this.filterHandler}
-        />
-        {this.state.bugs.map((bug) => (
+        <Nav filterHandler={this.filterHandler} />
+        {this.state.bugs.map(bug => (
           <BugTile
             bugName={bug.bugName}
             bugDescription={bug.bugDescription}
@@ -33,6 +41,7 @@ class App extends React.Component {
             createdDate={bug.createdDate}
             assignedTo={bug.assignedTo}
             threatLevel={bug.threatLevel}
+            filter={this.state.filter}
             key={bug.bugName}
           />
         ))}
