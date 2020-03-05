4a1846dbb99876045eb819d28a4551281c9072d6
Sean Lorman <sean.lorman@gmail.com>
2020-03-04 19:36:32 -0700

    Post bugs, confirmed through postman

--- server/db/queries.js
+++ server/db/queries.js
@@ -8,6 +8,23 @@ const sendBugs = () => {
   return pool.query("SELECT * FROM bug").then(res => res.rows);
 };
 
+const addBug = bug => {
+  const values = [
+    bug["bugDescription"],
+    bug["reportedBy"],
+    bug["createdDate"],
+    bug["assignedTo"],
+    bug["threatLevel"]
+  ];
+  return pool
+    .query(
+      "INSERT INTO bug (bugDescription, reportedBy, createdDate, assignedTo, threatLevel) VALUES ($1, $2, $3, $4, $5)",
+      values
+    )
+    .then(() => true);
+};
+
 module.exports = {
-  sendBugs
+  sendBugs,
+  addBug
 };
--- server/index.js
+++ server/index.js
@@ -1,13 +1,20 @@
 const express = require("express");
 const pool = require("./db/queries.js");
+const bp = require("body-parser");
 
 const app = express();
-const port = 8080;
+const port = 1212;
+
+app.use(bp.json());
 
 app.get("/api/bugs", (req, res) => {
   pool.sendBugs().then(results => {
     res.send(results);
   });
+});
+
+app.post("/api/bugs", (req, res) => {
+  pool.addBug(req.body).then(() => res.sendStatus(201));
 });
 
 app.listen(port, () => console.log(`Server app listening on port ${port}!`));
